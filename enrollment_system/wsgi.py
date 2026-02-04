46860e9039d2608740322759b096230afe002091
ramodjarod <ramosjarod04@gmail.com>
2026-02-05 02:35:22 +0800

    Update wsgi.py

--- enrollment_system/wsgi.py
+++ enrollment_system/wsgi.py
@@ -5,5 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enrollment_system.settings')
 
 application = get_wsgi_application()
 
-# This line is what Vercel is looking for
+# This is the "handler" Vercel is looking for
 app = application
