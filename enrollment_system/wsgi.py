eec50b09d62335ee6ce4a77f37695be13324ae76
reinelgiro123 <rci.bsis.competentegiro@gmail.com>
2026-02-05 01:55:48 +0800

    Slimmise

--- enrollment_system/wsgi.py
+++ enrollment_system/wsgi.py
@@ -1,9 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Ensure this matches your project folder name exactly
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enrollment_system.settings')
 
 application = get_wsgi_application()
 
-# Add this line for Vercel
+# Vercel looks for the variable 'app'
 app = application
