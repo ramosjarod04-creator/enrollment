ada18112e3f2f44ea2787e69cd4d00db3e8d84aa
ramodjarod <ramosjarod04@gmail.com>
2026-02-05 02:24:44 +0800

    Update wsgi.py

--- enrollment_system/wsgi.py
+++ enrollment_system/wsgi.py
@@ -5,11 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enrollment_system.settings')
 
 application = get_wsgi_application()
 
-# ADD THESE LINES AT THE BOTTOM
-from django.core.management import call_command
-try:
-    print("Attempting to run migrations...")
-    call_command('migrate', interactive=False)
-    print("Migrations successful!")
-except Exception as e:
-    print(f"Migration failed: {e}")
+# This line is what Vercel is looking for
+app = application
